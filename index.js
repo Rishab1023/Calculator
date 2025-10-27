be6eca1184f38bd28693a9db0af6bfbf84f58758
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-10-27 22:08:08 +0530

    Refactor calculator buttons and display text for improved functionality and layout

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const Acbtn = document.querySelector("#Ac"); //1
 const backspacebtn = document.querySelector("#backspace"); //2
-const btn9 = document.querySelector("#9"); //3
-const btn8 = document.querySelector("#8"); //4
-const btn7 = document.querySelector("#7"); //5
-const btn6 = document.querySelector("#6"); //6
-const btn5 = document.querySelector("#5"); //7
-const btn4 = document.querySelector("#4"); //8
-const btn3 = document.querySelector("#3"); //9
-const btn2 = document.querySelector("#2"); //10
-const btn1 = document.querySelector("#1"); //11
-const btn0 = document.querySelector("#0"); //12
-const btndot = document.querySelector("#.") //13
-const btndivide = document.querySelector("#/") //14
-const btnmultiply = document.querySelector("#*") //15
-const btnsubtract = document.querySelector("#-") //16
-const btnplus = document.querySelector("#+") //17
-const btnequal = document.querySelector("#=") //18
+const btnC = document.querySelector("#C"); //3
+const btndivide = document.querySelector("#divide"); //4
+const btn9 = document.querySelector("#nine"); //5
+const btn8 = document.querySelector("#eight"); //6
+const btn7 = document.querySelector("#seven"); //7
+const btnmultiply = document.querySelector("#x"); //8
+const btn6 = document.querySelector("#six"); //9
+const btn5 = document.querySelector("#five"); //10
+const btn4 = document.querySelector("#four"); //11
+const btnsubtract = document.querySelector("#subtract"); //12
+const btn3 = document.querySelector("#three") //13
+const btn2 = document.querySelector("#two") //14
+const btn1 = document.querySelector("#one") //15
+const btnadd = document.querySelector("#addition") //16
+const btnpercent = document.querySelector("#percentage") //17
+const btne0 = document.querySelector("#zero") //18
+const btndot = document.querySelector("#dot") //19
+const btnequal = document.querySelector("#equal") //20
+
+Acbtn.addEventListener("click",()=>{ buttonclick()});
 
 const buttonclick = () =>{
-
+  console.log("button was clicked");
+  
 }
