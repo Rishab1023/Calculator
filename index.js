59937469624fff4cfc70517fe16e06330f95ed23
Rishab1023 <rishabchaudhary3010@gmail.com>
2025-10-27 21:02:48 +0530

    Add initial HTML structure, CSS styles, and JavaScript functionality for Calculator App

---
+++ index.js
@@ -0,0 +1,22 @@
+const Acbtn = document.querySelector("#Ac"); //1
+const backspacebtn = document.querySelector("#backspace"); //2
+const btn9 = document.querySelector("#9"); //3
+const btn8 = document.querySelector("#8"); //4
+const btn7 = document.querySelector("#7"); //5
+const btn6 = document.querySelector("#6"); //6
+const btn5 = document.querySelector("#5"); //7
+const btn4 = document.querySelector("#4"); //8
+const btn3 = document.querySelector("#3"); //9
+const btn2 = document.querySelector("#2"); //10
+const btn1 = document.querySelector("#1"); //11
+const btn0 = document.querySelector("#0"); //12
+const btndot = document.querySelector("#.") //13
+const btndivide = document.querySelector("#/") //14
+const btnmultiply = document.querySelector("#*") //15
+const btnsubtract = document.querySelector("#-") //16
+const btnplus = document.querySelector("#+") //17
+const btnequal = document.querySelector("#=") //18
+
+const buttonclick = () =>{
+
+}
