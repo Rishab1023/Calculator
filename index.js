64cf3d36133e2e251099743db2ef0047133f1c58
Chaudhary Rishab Chandan Kumar <rishabchaudhary3010@gmail.com>
2025-12-28 10:34:04 +0530

    Fix button ID for zero and update event listeners in JavaScript for calculator functionality

--- index.js
+++ index.js
@@ -14,14 +14,40 @@ const btn3 = document.querySelector("#three") //13
 const btn2 = document.querySelector("#two") //14
 const btn1 = document.querySelector("#one") //15
 const btnadd = document.querySelector("#addition") //16
-const btnpercent = document.querySelector("#percentage") //17
-const btne0 = document.querySelector("#zero") //18
+const btnpercent = document.querySelector("#percent") //17
+const btn0 = document.querySelector("#zero") //18
 const btndot = document.querySelector("#dot") //19
 const btnequal = document.querySelector("#equal") //20
+const displayEl = document.querySelector("#display") // main display
 
-Acbtn.addEventListener("click",()=>{ buttonclick()});
+// function of the buttons
+Acbtn.addEventListener("click",()=>{ Acbtnclick()}); // clear all funtion
+backspacebtn.addEventListener("click",()=>{}) // backspace function
+btnC.addEventListener("click",()=>{}) // clear button function
+btndivide.addEventListener("click",()=>{}) // divide function
+btnmultiply.addEventListener("click",()=>{}) // multiply function
+btnadd.addEventListener("click",()=>{}) // add function
+btnsubtract.addEventListener("click",()=>{}) // subtract funtion
+btn9.addEventListener("click",()=>{}) // display 9 function
+btn8.addEventListener("click",()=>{}) // display 8 function
+btn7.addEventListener("click",()=>{}) // display 7 function
+btn6.addEventListener("click",()=>{}) // display 6 function
+btn5.addEventListener("click",()=>{}) // display 5 function
+btn4.addEventListener("click",()=>{}) // display 4 function
+btn3.addEventListener("click",()=>{}) // display 3 function
+btn2.addEventListener("click",()=>{}) // display 2 function
+btn1.addEventListener("click",()=>{}) // display 1 function
+btn0.addEventListener("click",()=>{}) // display 0 function
+btndot.addEventListener("click",()=>{}) // display dot function
+btnequal.addEventListener("click",()=>{}) // display = function
+btnpercent.addEventListener("click",()=>{}) // display % function
 
-const buttonclick = () =>{
+
+// functions for the buttons
+const Acbtnclick = () =>{
   console.log("button was clicked");
-  
+  displayEl.textContent = ""
+}
+const backspaceclick = () =>{
+  console.log("backspace");
 }
